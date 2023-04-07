406a851c06551c860aaddfd7a15da399250cd0b7
NohGaSeong <noyohanx@naver.com>
2023-04-07 19:18:13 +0900

    :sparkles: 회의공지 기능 기반 (wait_for)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import discord
 import os
+import asyncio
 
 from discord.ext import commands
 from dotenv import load_dotenv
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
 load_dotenv()
 Token = os.getenv('Token')
 
@@ -20,6 +23,20 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong")
 
+@bot.command()
+async def 회의공지(ctx):
+    await ctx.channel.send("회의를 할 장소를 15초내로 적어주세요.")
+
+    try:
+        message = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=15.0)
+    except asyncio.TimeoutError:
+        await ctx.channel.send("15초가 지났어요. 명령어를 다시 실행시켜주세요.")
+    
+    else :
+        if message.content is True :
+            await ctx.channel.send(message.content)
+        else :
+            await ctx.channel.send("장소를 다시 입력해주세요.")
 
 
 bot.run(Token)
