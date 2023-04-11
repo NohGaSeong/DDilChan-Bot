e981cf6e1148d28ea235d9efcd647f1d6f758646
yohan <noyohanx@naver.com>
2023-04-11 11:55:01 +0900

    :sparkle: 버튼 기본 세팅

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
-
+today_meet_count = 5
 
 load_dotenv()
 Token = os.getenv('Token')
 
 embed=discord.Embed(timestamp=datetime.now(pytz.timezone('UTC')), color=0x54b800)
 
+vivichan = "https://media.tenor.com/YLVttql_kgIAAAAC/%E3%82%94%E3%81%83%E3%81%A1%E3%82%83%E3%82%93-viichan.gif"
+vivivichan = "https://media.tenor.com/INrkO7KEe3QAAAAd/%EB%B9%84%EC%B1%A4-viichan.gif"
+many_metting_vichan = "https://media.tenor.com/L9C-SHIR2AQAAAAd/%EB%B9%84%EC%B1%A4-viichan.gif"
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -24,9 +27,30 @@ async def on_ready():
     print('------------')
     print(Token)
 
+class Menu(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    @discord.ui.button(label="회의 신청", style=discord.ButtonStyle.grey)
+    async def menu1(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message(content = "hello")
+
+    @discord.ui.button(label="회의 목록", style = discord.ButtonStyle.blurple)
+    async def menu2(self, interaction: discord.Interaction, button : discord.ui.Button):
+        embed = discord.Embed(color=discord.Color.random())
+        embed.set_author(name=f"This is a edited embed")
+        
+        await interaction.response.send_message(embed=embed)
+    
+    @discord.ui.button(label="명령어", style = discord.ButtonStyle.red)
+    async def menu3(self, interaction: discord.Interaction, button : discord.ui.Button):
+        await interaction.response.send_message("Hello World")
+
 @bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+async def 띨챤(ctx):
+    view = Menu()
+    await ctx.reply("챤하 ~ 무엇을 도와드릴까요?", view=view)
 
 @bot.command()
 async def 회의공지(ctx):
