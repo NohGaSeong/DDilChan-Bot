e5dd819ca264b346f68a07b52b7f7321ae3c6977
yohan <noyohanx@naver.com>
2023-04-11 13:17:12 +0900

    :sparkles: class들 기본 뼈대 잡아놨음

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ many_metting_vichan_gif = "https://media.tenor.com/L9C-SHIR2AQAAAAd/%EB%B9%84%EC
 many_many_metting_vichan_gif = "https://media.tenor.com/INrkO7KEe3QAAAAd/%EB%B9%84%EC%B1%A4-viichan.gif"
 one_metting_vichan_gif = "https://media.tenor.com/INrkO7KEe3QAAAAd/%EB%B9%84%EC%B1%A4-viichan.gif"
 no_metting_vichan_gif = "https://media.tenor.com/YLVttql_kgIAAAAC/%E3%82%94%E3%81%83%E3%81%A1%E3%82%83%E3%82%93-viichan.gif"
+
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -30,6 +32,34 @@ async def on_ready():
     print('------------')
     print(Token)
 
+class Metting_member(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+    
+    @discord.ui.button(label="멤버선택",  style=discord.ButtonStyle.grey)
+    async def member_select(self, interaction:discord.Interaction, button:discord.ui.button):
+        await interaction.response.send_message(content = "회의 등록이 완료됐어요.")
+
+class Metting_time(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+    @discord.ui.button(label= "아침시간", style=discord.ButtonStyle.grey)
+    async def metting_time_1(self, interaction:discord.Interaction, button:discord.ui.button):
+        view = Metting_member()
+        await interaction.response.send_message(content = "회의 등록이 완료되었어요!",view=view)
+
+class Metting_place(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    @discord.ui.button(label="2층 홈베이스", style=discord.ButtonStyle.grey)
+    async def metting_place_1(self, interaction:discord.Interaction, button : discord.ui.button):
+        view = Metting_time()
+        await interaction.response.send_message(content= "회의할 시간을 선택해주세요", view=view)
+
 class Menu(discord.ui.View):
     def __init__(self):
         super().__init__()
@@ -37,12 +67,16 @@ class Menu(discord.ui.View):
 
     @discord.ui.button(label="회의 신청", style=discord.ButtonStyle.grey)
     async def menu1(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message(content = "hello")
+        view = Metting_place()
+        member = interaction.user
+        await interaction.response.send_message(content = "회의를 할 장소를 골라주세요.", view=view)
+
 
     @discord.ui.button(label="회의 목록", style = discord.ButtonStyle.blurple)
     async def menu2(self, interaction: discord.Interaction, button : discord.ui.Button):
         embed = discord.Embed(color=discord.Color.random())
         embed.set_author(name=f"This is a edited embed")
+        print(interaction.author)
 
         if today_meet_count > 3:
             embed.set_image(url=(many_many_metting_vichan_gif))
@@ -71,24 +105,6 @@ async def 띨챤(ctx):
     view = Menu()
     await ctx.reply("챤하 ~ 무엇을 도와드릴까요?", view=view)
 
-@bot.command()
-async def 회의공지(ctx):
-    await ctx.channel.send("회의를 할 장소를 15초내로 적어주세요.")
-
-    try:
-        message = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=15.0)
-    except asyncio.TimeoutError:
-        await ctx.channel.send("15초가 지났어요. 명령어를 다시 실행시켜주세요.")
-    
-    else :
-        if message.content is True :
-            await ctx.channel.send(message.content)
-        else :
-            await ctx.channel.send("장소를 다시 입력해주세요.")
-
-@bot.command()
-async def 회의확인(ctx):
-    await ctx.channel.send(file=discord.File("resource\회의확인.png"), embed=embed)
 
 
 
